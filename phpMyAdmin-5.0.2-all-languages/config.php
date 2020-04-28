639aae36548c2ba4a1e6ad062d9a7fe414d2366a
Nikolaj Petersen <36536682+Nikofisk@users.noreply.github.com>
2020-04-28 11:44:44 +0200

    Create config.php

---
+++ phpMyAdmin-5.0.2-all-languages/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+ini_set('display_errors', 'On');
+
+define ("DB_NAME", "testtestesen");
+define ("DB_USERNAME", "testtestesen");
+define ("DB_PASSWORD", "Qp3Hd~E8qgY_");
+define ("DB_HOST", "mysql2.gear.host");
+
+?>
